45c5b02e6bcbe47bb7de8b70f6f02b61d97b747c
Angus Woodman <angus.woodman@outlook.comgit config --global user.name yourGithubUsernameAngus-Woodman>
2020-09-02 15:52:57 +0100

    get postContainer setup

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import "./styles/App.css";
+import PostContainer from "./Containers/PostContainer"
+
 class App extends React.Component {
   state = {};
   render() {
     return (
       <>
         <h1>New React App</h1>
-        <h2>Happy Coding</h2>
+        <PostContainer/>
       </>
     );
   }
---
+++ Client/src/Containers/postContainer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class PostContainer extends Component {
+  render(){
+        return(
+            <section id="postContainer">
+              <h1>Test</h1>
+            </section>
+        )
+    }
+}
+
+export default PostContainer;
